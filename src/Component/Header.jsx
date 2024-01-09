2ea1014bf408d27eba9c7dfd3231c25806f011ac
aimad-hasnaoui <aimadha1234@gmail.com>
2024-01-09 09:39:36 +0100

    new

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,9 +1,10 @@
-
+import Team from '../assets/Team-E.png'
 
 export default function Header() {
   return (
     <div className="w-full flex items-center justify-between px-10 py-8 bg-[#FFFFFF] shadow-md rounded-2xl">
-      <h1 className="text-[25px] text-[#56299F] font-bold">Team-e</h1>
+      {/* <h1 className="text-[25px] text-[#56299F] font-bold">Team-e</h1> */}
+      <img src={Team} alt="" className='w-[70px]' />
       <div className="flex gap-12 px-5">
         <a href="" className="font-semibold">About</a>
         <a href="" className="font-semibold">contact</a>
